c1fca89200809cdc0a5e601becd49555e05e33e2
Nikhildavis123 <nikhildavisth@gmail.com>
2025-06-04 09:52:22 +0200

    adding requirements.txt

--- ParkingFeeBackend/path.py
+++ ParkingFeeBackend/path.py
@@ -1,3 +1,3 @@
-model_path       = r"C:\Users\clint\OneDrive\coding\Git_computer_vision\Parking-Fee-ComputerVision\ParkingFeeBackend\number_plate_detection.xml"
-ip_camera        ="http://192.168.178.65:4747/video"
+model_path       = r"C:\Users\akhil\Desktop\nik\Parking-Fee-ComputerVision\ParkingFeeBackend\number_plate_detection.xml"
+ip_camera        = "http://192.168.178.65:4747/video"
 saved_plate_path = r"C:\Users\clint\OneDrive\coding\plates" 
